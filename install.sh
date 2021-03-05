f02aa3328dbc6404d03e8181a8c7d33d3072853d
Benjamin Arntzen <zorlin@gmail.com>
2021-03-06 07:49:03 +0800

    Add SSH and keyscan stuff, turn on automatic interp discovery

--- install.sh
+++ install.sh
@@ -7,3 +7,10 @@ sudo pip3 install ansible molecule
 sudo wget -nc -c "https://github.com/mitogen-hq/mitogen/archive/v0.3.0-rc.0.zip" -O /opt/v0.3.0-rc.0.zip
 sudo unzip -oq /opt/v0.3.0-rc.0.zip
 sudo ln -s /opt/mitogen-0.3.0-rc.0/ /opt/mitogen
+# Enable ssh
+sudo apt install -y openssh-server
+sudo systemctl enable ssh
+sudo systemctl start ssh
+sudo cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+# Scan the local key
+sudo ssh-keyscan -H localhost >> ~/.ssh/known_hosts
